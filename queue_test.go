b6668536d25bfea411cc32f4a7163f2a546f51e0
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-11-14 17:05:31 -0500

    Adds Queue implementations using linked list and slice

---
+++ queue_test.go
@@ -0,0 +1,82 @@
+package gotoolkit
+
+import (
+	"testing"
+)
+
+func TestEnqueue(t *testing.T) {
+	queues := []Queue{new(ListQueue), new(SliceQueue)}
+
+	for _, queue := range queues {
+		queue.Enqueue("Test")
+
+		if queue.Size() == 0 {
+			t.Errorf("Incorrect result\ngot:  %d\nwant: %d", queue.Size(), 0)
+		}
+	}
+
+}
+
+func TestDequeue(t *testing.T) {
+	queues := []Queue{new(ListQueue), new(SliceQueue)}
+
+	for _, queue := range queues {
+		queue.Enqueue("Test")
+		item, err := queue.Dequeue()
+
+		if err != nil {
+			t.Errorf("Incorrect result\ngot:  %v\nwant: %v", err, nil)
+		}
+
+		if item != "Test" {
+			t.Errorf("Incorrect result\ngot:  %s\nwant: %s", item, "Test")
+		}
+
+		if queue.Size() != 0 {
+			t.Errorf("Incorrect result\ngot:  %d\nwant: %d", queue.Size(), 0)
+		}
+	}
+}
+
+func TestDequeueWithEmptyQueue(t *testing.T) {
+	queues := []Queue{new(ListQueue), new(SliceQueue)}
+
+	for _, queue := range queues {
+		item, err := queue.Dequeue()
+		want := "unable to dequeue element, queue is empty"
+
+		if err.Error() != want {
+			t.Errorf("Incorrect result\ngot:  %s\nwant: %s", err, want)
+		}
+
+		if item != nil {
+			t.Errorf("Incorrect result\ngot:  %v\nwant: %v", item, nil)
+		}
+
+		if !queue.IsEmpty() {
+			t.Errorf("Incorrect result\ngot:  %t\nwant: %t", queue.IsEmpty(), true)
+		}
+	}
+}
+
+func TestPeekQueue(t *testing.T) {
+	queues := []Queue{new(ListQueue), new(SliceQueue)}
+
+	for _, queue := range queues {
+		queue.Enqueue("Test")
+
+		item, err := queue.Peek()
+
+		if err != nil {
+			t.Errorf("Incorrect result\ngot:  %v\nwant: %v", err, nil)
+		}
+
+		if item != "Test" {
+			t.Errorf("Incorrect result\ngot:  %s\nwant: %s", item, "Test")
+		}
+
+		if queue.Size() != 1 {
+			t.Errorf("Incorrect result\ngot:  %d\nwant: %d", queue.Size(), 1)
+		}
+	}
+}
